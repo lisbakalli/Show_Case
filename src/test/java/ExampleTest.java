58ad1b0c65369e6ca90b2b6aa80dd5dcc9ab7331
Lis Bakalli <lbakalli@deloitte.de>
2020-04-29 14:54:54 +0200

    Added initial test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,2 +1,24 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class ExampleTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void before() {
+        System.out.println("Before Test Case");
+    }
+
+    @Test
+    public void addTwoandTwo() {
+        System.out.println("Test 2 + 2");
+        assertTrue("add(2,2) equals 4", ExampleClass.add(2, 2) == 4.0);
+    }
 }
