54c0d20f34c1d8608b5a35dab9f741eff4dbf293
Lis Bakalli <lbakalli@deloitte.de>
2020-04-29 14:46:35 +0200

    First push

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,9 @@
+public class ExampleClass {
+    public static double add(double numberOne, double numbreTwo) {
+        return numberOne + numbreTwo;
+    }
+
+    public static double subtract(double numberOne, double numbreTwo) {
+        return numberOne - numbreTwo;
+    }
+}
